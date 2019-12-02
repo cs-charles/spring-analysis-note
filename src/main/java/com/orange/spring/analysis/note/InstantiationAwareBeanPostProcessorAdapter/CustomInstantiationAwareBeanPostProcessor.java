716fcbe4a9c4b96143d81827de50835275566eb7
xieyong <xieyong@souche.com>
2019-12-02 10:29:15 +0800

    IABPPA接口解析

---
+++ src/main/java/com/orange/spring/analysis/note/InstantiationAwareBeanPostProcessorAdapter/CustomInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,12 @@
+package com.orange.spring.analysis.note.InstantiationAwareBeanPostProcessorAdapter;
+
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
+
+/**
+ * @author xieyong
+ * @date 2019/12/2
+ * @Description:
+ */
+public class CustomInstantiationAwareBeanPostProcessor extends InstantiationAwareBeanPostProcessorAdapter {
+
+}
